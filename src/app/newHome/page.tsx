19d6c0535761f2b61c64430589903a33cf69ffc9
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-02-18 21:44:12 +0530

    update new home

--- src/app/newHome/page.tsx
+++ src/app/newHome/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { Auction } from "@prisma/client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function AuctionCard({ title, description, startTime, endTime }: Auction) {
+  return <div>{/* display the auction card */}</div>;
+}
+
 export default function Home() {
-  return <div>Home</div>;
+  const [auctions, setAuctions] = useState<Auction[]>([]);
+  console.log(auctions);
+
+  const currentTime = new Date();
+
+  const liveAuctions = auctions.filter(
+    (auction) => currentTime >= auction.startTime
+  );
+
+  const upcomingAuctions = auctions.filter(
+    (auction) => currentTime < auction.startTime
+  );
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get("/api/getAllAuctions");
+      setAuctions(res.data.auctions);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <section className="live-auctions"></section>
+      <section className="upcoming-auctions"></section>
+    </div>
+  );
 }
