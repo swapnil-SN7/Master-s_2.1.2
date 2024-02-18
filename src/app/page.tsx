60accb819b6bc5299775c7dd58ba71b8813b06d5
Mastermind730 <sourav.97032@gmail.com>
2024-02-18 12:44:08 +0530

    changed layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 
 export default function Home() {
   return(     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-    <Navbar/>
+    {/* <Navbar/> */}
     </main>
 
   )
