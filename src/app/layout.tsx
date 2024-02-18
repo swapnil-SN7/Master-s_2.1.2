7b65a35c3dc680485a5072f97c61629775fec231
swapnil-SN7 <swapnilwalave96@gmail.com>
2024-02-18 12:31:03 +0530

    start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "../../component/Navbar";
-import  Footer from "../../component/Footer";
+// import Navbar from "../../component/Navbar";
+// import  Footer from "../../component/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        {/* <Navbar/> */}
         {children}
-        <Footer/>
+        {/* <Footer/> */}
         </body>
     </html>
   );
---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+
+
+// import Lottie from 'reac-lottie'
+// import animationData from './assets/notfound';
+const Notfound = ()=>{
+    return (
+        <div>
+
+       <h1>NotFound</h1>
+       <div></div>
+            
+        </div>
+    )
+}
+
+export default Notfound;
