103975fb9a12f563a08d1e5b6b750d5bd0f1aa57
Mastermind730 <sourav.97032@gmail.com>
2024-02-18 13:22:47 +0530

    fixed some styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <Navbar/>
+
       <body className={inter.className}>{children}</body>
 
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,75 @@
-
-
-
-
-'use client'
+"use client";
 
 import { ChangeEvent, useState } from "react";
 import { FaSearchDollar } from "react-icons/fa";
-import Image from "next/image"
-import './globals.css'
+import Image from "next/image";
+import "./globals.css";
+import Navbar from "./components/Navbar";
 export default function Home() {
+  const [activeSearch, setActiveSearch] = useState([]);
 
-  const [activeSearch, setActiveSearch] = useState([])
-
-   
   function handleSearch(e: ChangeEvent<HTMLInputElement>): void {
     throw new Error("Function not implemented.");
   }
 
-  return(
-    <div>
-
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <Navbar/>
+      <div className="container mx-auto mt-1 px-4 sm:px-6 lg:px-8">
 
       <div className="part_one  mx-auto  mt-20 border-gray-300 border relative bg-[#ccaafd] pb-32  ">
-
         <div className="searchbar  flex justify-center mb-8  absolute -top-5 left-96">
-              
-      <form className='w-[500px] relative'>
-        <div className="relative">
-            <input type="search" placeholder='Type Here' className='w-full text-cyan-50 p-4 bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
-               ' onChange={(e) => handleSearch(e)}/>
-            <button className='absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-[#6900FF] rounded-full'>
+          <form className="w-[500px] relative">
+            <div className="relative">
+              <input
+                type="search"
+                placeholder="Type Here"
+                className="w-full text-cyan-50 p-4 bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
+               "
+                onChange={(e) => handleSearch(e)}
+              />
+              <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-[#6900FF] rounded-full">
                 <FaSearchDollar />
-            </button>
+              </button>
+            </div>
+          </form>
         </div>
-        
-        </form>
-        </div>
-
 
         <div className="flex flex-row justify-center  mt-24">
+          <div className="leftpart pr-20 ">
+            <p className="text-2xl font-extrabold text-opacity-80 text-[#6900FF]">
+              "Unlock the thrill of winning with every click – bid, win,
+              repeat!"
+            </p>
 
-        <div className="leftpart pr-20 ">
-             <p  className="text-2xl font-extrabold text-opacity-80 text-[#6900FF]">
-"Unlock the thrill of winning with every click – bid, win, repeat!"</p>
+            <button className="border-white bottom-2  bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border  m-4 p-2 flex justify-around content-baseline">
+              <div className="bg-[#FFD700] size-3  rounded-full m-1 "></div>
+              <span> Trending NFT's🔥</span>
+            </button>
 
-         <button  className="border-white bottom-2  bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border  m-4 p-2 flex justify-around content-baseline">
-             <div className="bg-[#FFD700] size-3  rounded-full m-1 "></div>
-        <span>  Trending NFT's🔥</span>
-          </button>
-
-          <button  className="bottom-2  bg-blue-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border  m-1 p-2 ml-3  ">
-       
+            <button className="bottom-2  bg-blue-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border  m-1 p-2 ml-3  ">
               Time left -10:30 min
-          </button>
+            </button>
+          </div>
 
+          <div className="rightpart">
+            <Image alt="" height={30} width={30} src="/images/filter.png" />
 
+            <div className="dropdown dropdown-right ml-4 flex">
+              <button className="border-white bottom-2  text-center bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border  m-4 p-2 flex justify-around content-baseline">
+                <p className="text-sm"> Highest Bid🔥</p>
+              </button>
+
+              <button className="border-white bottom-2  bg-[#6900FF]    rounded-full   backdrop-blur-sm  border  m-4 p-2 ">
+                <span className=" text-2xl font-semibold text-whitest">
+                  Place Bid
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
-        
-
-        <div className="rightpart">
-
-           <Image alt="" height={30} width={30} src="/images/filter.png"/>
-
-
-                 <div className="dropdown dropdown-right ml-4 flex">
-                 <button  className="border-white bottom-2  text-center bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border  m-4 p-2 flex justify-around content-baseline">
-             <p className="text-sm"> Highest Bid🔥</p>
-              
-               </button>
-     
-               <button  className="border-white bottom-2  bg-[#6900FF]    rounded-full   backdrop-blur-sm  border  m-4 p-2 ">
-                  
-             <span className=" text-2xl font-semibold text-whitest">Place Bid</span>
-               </button>
-                  
-                  </div>           
-           
-
-
-
-        </div>
-
-
-        </div>
-
-
-
-
-
       </div>
-
-
-
-
-          
-    </div>
-
-
-    
+      </div>
+    </main>
   );
 }
