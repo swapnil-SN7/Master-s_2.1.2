bf21d1ccb93030cdc6e9f39f5f198182677aab70
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-02-19 15:06:25 +0530

    info msg

--- src/app/additem/page.tsx
+++ src/app/additem/page.tsx
@@ -32,6 +32,8 @@ const Page = () => {
   const [formSuccess, setFormSuccess] = useState(false);
   const [formSuccessMessage, setFormSuccessMessage] = useState("");
 
+  const [infoMsg, setInfoMsg] = useState("");
+
   const handleInput = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
   ) => {
@@ -82,6 +84,13 @@ const Page = () => {
       console.log("AN unexpected error occured");
       // setError("An unexpected error occured");
     }
+    setFormData({
+      auc_id: "",
+      base_price: 0,
+      description: "",
+      name: "",
+      tag: "",
+    });
   };
 
   return (
@@ -92,86 +101,111 @@ const Page = () => {
           {formSuccessMessage}
         </div>
       ) : (
-        <form
-          method="POST"
-          action="/api/addItemToAuction"
-          onSubmit={submitForm}
-          className="mb-4"
-        >
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              onChange={handleInput}
-              value={formData.name}
-              className="w-full px-3 py-2 border rounded-md"
-            />
-          </div>
+        <div></div>
+      )}
+      <form
+        method="POST"
+        action="/api/addItemToAuction"
+        onSubmit={submitForm}
+        className="mb-4"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleInput}
+            value={formData.name}
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="description"
-            >
-              Description
-            </label>
-            <textarea
-              name="description"
-              onChange={handleInput}
-              value={formData.description}
-              className="w-full px-3 py-2 border rounded-md"
-            ></textarea>
-          </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            name="description"
+            onChange={handleInput}
+            value={formData.description}
+            className="w-full px-3 py-2 border rounded-md"
+          ></textarea>
+        </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="tag"
-            >
-              Tag
-            </label>
-            <select
-              name="tag"
-              onChange={handleInput}
-              value={formData.tag}
-              className="w-full px-3 py-2 border rounded-md"
-            >
-              <option value="">Select Tag</option>
-              <option value="Arts">Arts</option>
-              <option value="Collectables">Collectables</option>
-              {/* Add more options based on your requirements */}
-            </select>
-          </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="tag"
+          >
+            Tag
+          </label>
+          <select
+            name="tag"
+            onChange={handleInput}
+            value={formData.tag}
+            className="w-full px-3 py-2 border rounded-md"
+          >
+            <option value="">Select Tag</option>
+            <option value="Arts">Arts</option>
+            <option value="Collectables">Collectables</option>
+            {/* Add more options based on your requirements */}
+          </select>
+        </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="base_price"
-            >
-              Base Price
-            </label>
-            <input
-              type="number"
-              name="base_price"
-              onChange={handleInput}
-              value={formData.base_price}
-              className="w-full px-3 py-2 border rounded-md"
-            />
-          </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="base_price"
+          >
+            Base Price
+          </label>
+          <input
+            type="number"
+            name="base_price"
+            onChange={handleInput}
+            value={formData.base_price}
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
 
+        <div className="flex justify-between">
           <button
             type="submit"
             className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300"
           >
             Add Item
           </button>
-        </form>
+
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+            onClick={(e) => {
+              setInfoMsg(
+                `Auction scheduled successfully from ${new Date(
+                  Date.parse(auc_start_time as string)
+                ).toLocaleString()} to ${new Date(
+                  Date.parse(auc_end_time as string)
+                ).toLocaleDateString()}`
+              );
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      {infoMsg !== "" ? (
+        <div className="rounded border border-green-500 text-green-500 p-1 text-center">
+          {infoMsg}
+        </div>
+      ) : (
+        <div></div>
       )}
     </div>
   );
