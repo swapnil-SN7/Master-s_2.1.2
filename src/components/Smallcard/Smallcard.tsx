0e2927f80077cc22aa5c015e631320c022b3590f
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-02-19 15:50:04 +0530

    fix errors

--- src/components/Smallcard/Smallcard.tsx
+++ src/components/Smallcard/Smallcard.tsx
@@ -1,36 +1,30 @@
-'use client'
+"use client";
 
-const Smallcard = ()=>{
+import Image from "next/image";
 
-    return(
-
-     
-
-              <div className="card flex gap-3 border rounded-lg border-[#AEE2FF] shadow-xl drop-shadow-lg p-4">
-                <div className="part1 ">
-                    <div>
-                        <img src="/images/auction.png" alt=""  height={50} width={50}/>
-                        <div>
-                            <p className="font-light">Auction:Type</p>
-                            <p className=" font-bold"> NFT  Name</p>
-                        </div>
-                    </div>
-                    <button className="w-full border rounded-lg border-blue-200 p-1 ">
-                        <p className="text-sm">Ends in :  5d 6h 9m 37s</p>
-                    </button>
-
-                </div>
-                <div className="part2">
-                    <h2 className="font-bold text-blue-700 text-xl mb-[9%]">1.26 ETH</h2>
-                    <button className="border tracking tracking-wide border-[#AEE2FF] hover:border-[#B799FF] rounded-2xl bg-blue-500 p-1 ml-3" >
-            <p className="text-md font-bold text-white">Place Bid</p>
+const Smallcard = () => {
+  return (
+    <div className="card flex gap-3 border rounded-lg border-[#AEE2FF] shadow-xl drop-shadow-lg p-4">
+      <div className="part1 ">
+        <div>
+          <Image src="/images/auction.png" alt="" height={50} width={50} />
+          <div>
+            <p className="font-light">Auction:Type</p>
+            <p className=" font-bold"> NFT Name</p>
+          </div>
+        </div>
+        <button className="w-full border rounded-lg border-blue-200 p-1 ">
+          <p className="text-sm">Ends in : 5d 6h 9m 37s</p>
         </button>
-                </div>
+      </div>
+      <div className="part2">
+        <h2 className="font-bold text-blue-700 text-xl mb-[9%]">1.26 ETH</h2>
+        <button className="border tracking tracking-wide border-[#AEE2FF] hover:border-[#B799FF] rounded-2xl bg-blue-500 p-1 ml-3">
+          <p className="text-md font-bold text-white">Place Bid</p>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-              </div>
-       
-    )
-
-}
-
-export  default Smallcard;
+export default Smallcard;
