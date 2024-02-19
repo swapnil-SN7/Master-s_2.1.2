6f5b940bc2bc2ff5b633fb6ea84c1b3bc4825f3c
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-02-19 09:02:07 +0530

    fix

--- src/app/newHome/page.tsx
+++ src/app/newHome/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Dropdown from "@/components/Dropdown";
 import { Auction } from "@prisma/client";
 import axios from "axios";
@@ -10,8 +11,6 @@ function AuctionCard({ title, description, startTime, endTime }: Auction) {
 }
 
 export default function Home() {
-
-  
   const [auctions, setAuctions] = useState<Auction[]>([]);
   console.log(auctions);
 
@@ -37,123 +36,116 @@ export default function Home() {
         setAuctions(res.data.auctions);
       } catch (error) {
         // Handle the error, e.g., log it or show a user-friendly message
-        
       }
     };
-  
+
     fetchData();
   }, []);
-  
+
   return (
-    <Landing/>>
+    <>
+      <Landing />
 
+      <div className="w-11/12 mx-auto p-7">
+        <div></div>
+        <div className="All_data flex justify-between items-center gap-4">
+          <div className="leftpart">
+            <p className="p-2 font-extrabold text-2xl text-[#6900FF]">
+              Live Auction
+            </p>
+            <button className="btn border rounded-xl  border-blue-500 ring-yellow-500 p-1 ">
+              ^$# Live Auction
+            </button>
 
+            <div className="flex justify-center gap-2">
+              <Dropdown val={"Sort"} />
+              <Dropdown val={"Filter"} />
+            </div>
 
-<div className="w-11/12 mx-auto p-7">
-
-
-<div>
-  
-
-
-</div> 
-  <div className="All_data flex justify-between items-center gap-4">
-
-  <div className="leftpart">
-  <p className="p-2 font-extrabold text-2xl text-[#6900FF]">Live Auction</p> 
-   <button className="btn border rounded-xl  border-blue-500 ring-yellow-500 p-1 ">
-       ^$# Live Auction
-   </button>
-      
-
-      <div className="flex justify-center gap-2">
-      <Dropdown val={"Sort"}/>
-     <Dropdown val={"Filter"}/>
-      </div>
-     
-     <div className=" grid-cols-2 grid-flow-row gap-y-8 gap-x-4 sm:grid">
-     {liveAuctions.map((item, index) => (
-             
-                      <div key={index} className="card relative  h-[80%] w-[95%] bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 p-7 m-4 content-center mx-auto" >
-                      <div className="content-center font-medium mx-auto"  > 
-                    <p className="font-medium text-blue-400 text-xl">{item.title}</p>
-                      </div>
-                        <div className="ds mx-auto">
-                          <h1 className=" font-bold text-sm">Description</h1>
-                          <p>{item.description}</p>
-                        </div>
-          
-                        <div className="flex justify-around gap-4 m-4 border rounded-md p-2 border-[#AEE2FF]">
-          <p className="text-sm">Start Date;- {item.startTime.toString()}</p>
-          <p className="text-sm">End Date;- {item.endTime.toString()}</p>
-          </div>
-          
-              <div className=" mx-auto">
-              <button className="border tracking tracking-wide border-[#AEE2FF] hover:border-[#B799FF] rounded-lg m-[2%]" >
-                      <p className="text-base p-1 text-blue-500">View Collections</p>
-                  </button>
-                                  
-              </div>
-            
-          
+            <div className=" grid-cols-2 grid-flow-row gap-y-8 gap-x-4 sm:grid">
+              {liveAuctions.map((item, index) => (
+                <div
+                  key={index}
+                  className="card relative  h-[80%] w-[95%] bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 p-7 m-4 content-center mx-auto"
+                >
+                  <div className="content-center font-medium mx-auto">
+                    <p className="font-medium text-blue-400 text-xl">
+                      {item.title}
+                    </p>
                   </div>
- 
+                  <div className="ds mx-auto">
+                    <h1 className=" font-bold text-sm">Description</h1>
+                    <p>{item.description}</p>
+                  </div>
 
+                  <div className="flex justify-around gap-4 m-4 border rounded-md p-2 border-[#AEE2FF]">
+                    <p className="text-sm">
+                      Start Date;- {item.startTime.toString()}
+                    </p>
+                    <p className="text-sm">
+                      End Date;- {item.endTime.toString()}
+                    </p>
+                  </div>
 
-          ))}
+                  <div className=" mx-auto">
+                    <button className="border tracking tracking-wide border-[#AEE2FF] hover:border-[#B799FF] rounded-lg m-[2%]">
+                      <p className="text-base p-1 text-blue-500">
+                        View Collections
+                      </p>
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
 
+        <div className="rightpart">
+          <div className="grid grid-cols-2 grid-flow-row gap-y-6 p-7 border rounded-lg ring-2 drop-shadow-xl content-center   w-[50%] mx-auto  mt-[7%]  mb-[4%]">
+            <h2 className="font-bold text-[#6900FF] text-lg">
+              Upcoming Auction
+            </h2>
 
-     </div>
-
-  </div>
-
-
-  </div>
-
-  <div className="rightpart">
-   <div className="grid grid-cols-2 grid-flow-row gap-y-6 p-7 border rounded-lg ring-2 drop-shadow-xl content-center   w-[50%] mx-auto  mt-[7%]  mb-[4%]">
-   <h2 className="font-bold text-[#6900FF] text-lg">Upcoming Auction</h2>
-
-   {upcomingAuctions.map((item, index) => (
-
-              <div  key={index} className="card flex gap-3 border rounded-lg border-[#AEE2FF] shadow-xl drop-shadow-lg p-4">
-              <div className="part1 ">
+            {upcomingAuctions.map((item, index) => (
+              <div
+                key={index}
+                className="card flex gap-3 border rounded-lg border-[#AEE2FF] shadow-xl drop-shadow-lg p-4"
+              >
+                <div className="part1 ">
                   <div>
-                      <img src="/images/auction.png" alt=""  height={50} width={50}/>
-                      <div>
-                          <p className="font-light">{item.id}</p>
-                          <p className=" font-bold"> {item.title}</p>
-                      </div>
+                    <img
+                      src="/images/auction.png"
+                      alt=""
+                      height={50}
+                      width={50}
+                    />
+                    <div>
+                      <p className="font-light">{item.id}</p>
+                      <p className=" font-bold"> {item.title}</p>
+                    </div>
                   </div>
                   <button className="w-full border rounded-lg border-blue-200 p-1 ">
-                      <p className="text-sm">Starts on :  {item.startTime.toString()}</p>
-                      <p className="text-sm">Ends on :  {item.endTime.toString()}</p>
-                      
+                    <p className="text-sm">
+                      Starts on : {item.startTime.toString()}
+                    </p>
+                    <p className="text-sm">
+                      Ends on : {item.endTime.toString()}
+                    </p>
                   </button>
-
+                </div>
+                <div className="part2">
+                  <h2 className="font-bold text-blue-700 text-xl mb-[9%]">
+                    {item.organiserId}
+                  </h2>
+                  <button className="border tracking tracking-wide border-[#AEE2FF] hover:border-[#B799FF] rounded-2xl bg-blue-500 p-1 ml-3">
+                    <p className="text-md font-bold text-white">Place Bid</p>
+                  </button>
+                </div>
               </div>
-              <div className="part2">
-                  <h2 className="font-bold text-blue-700 text-xl mb-[9%]">{item.organiserId}</h2>
-                  <button className="border tracking tracking-wide border-[#AEE2FF] hover:border-[#B799FF] rounded-2xl bg-blue-500 p-1 ml-3" >
-              <p className="text-md font-bold text-white">Place Bid</p>
-              </button>
-              </div>
-
-              </div>
-
-              
-             ))}
-
-      
-      
-
-       
-   </div>
-</div>
-
-
- </div>
-
-
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
